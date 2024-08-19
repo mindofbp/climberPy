ff298c9c98b95ad0f0b3a36fe9eefafaa19c5686
Bryce Parry <65125426+mindofbp@users.noreply.github.com>
2024-08-19 13:53:16 -0700

    feat: very basic CLI

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from classes import *
 import identifiers
+import os
 import parsers
+import sys
 
 if __name__ == "__main__":
-    # points = parsers.parse_gpx_file(
-    #     "Triple_Zoo_Hill_Climb_-_Return_Mercer_Island_metric.gpx"
-    # )
-    points = parsers.parse_fit_file("upper_lake_loop.fit")
+    input_file = sys.argv[1]
+
+    if input_file[-4:] != ".fit":
+        raise ValueError("Invalid file type. Please provide a .fit")
+
+    if os.path.isfile(input_file) == False:
+        raise ValueError("File does not exist. Please provide a valid file")
+
+    points = parsers.parse_fit_file(input_file)
     climbs = identifiers.ClimbIdentifier(points).identify_climbs()
     for index, segment in enumerate(climbs):
         if segment.distance >= 1000:
             # If distance is less than 2k then we wil plot in 400m segments
-            # If the distance is betwen 2k and 5k then we will plot in 800m segments
+            # If the distance is between 2k and 5k then we will plot in 800m segments
             # If the distance is greater than 5k then we will plot in 1k segments
-            if segment.distance < 2000:
-                segment.plot_segment(400, index)
-            elif segment.distance < 5000:
-                segment.plot_segment(800, index)
+            if segment.distance < 5000:
+                segment.plot_segment(500, index)
             else:
                 segment.plot_segment(1000, index)
